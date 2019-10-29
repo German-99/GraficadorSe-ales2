2af10fdaf7798e9796800cdcecfa27d708a88ec0
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-29 11:48:31 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/SeñalExponencial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalExponencial : Señal
+    {
+        public double Alpha { get; set; }
+
+        public SeñalExponencial()
+        {
+            Alpha = 0.0;
+            Muestras = new List<Muestra>();
+        }
+        public SeñalExponencial(double alpha)
+        {
+            Alpha = alpha;
+            Muestras = new List<Muestra>();
+        }
+
+        override public double evaluar(double tiempo)
+        {
+            double resultado = 0.0;
+            resultado = Math.Exp(Alpha * tiempo);
+            return resultado;
+        }
+    }
+}
